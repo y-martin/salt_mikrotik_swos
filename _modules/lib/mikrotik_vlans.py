afbe0f1f202369ef072c003d67112984a38b70ff
yan <yannick.martin@okazoo.eu>
2023-05-12 16:07:11 +0200

    implem

--- _modules/lib/mikrotik_vlans.py
+++ _modules/lib/mikrotik_vlans.py
@@ -77,7 +77,7 @@ class Mikrotik_Vlans(Swostab):
     def save(self):
         i = 0
         while i < len(self._data):
-            vlan_id = int(i['vid'], 16)
+            vlan_id = int(self._data[i]['vid'], 16)
             self._update_data(i, utils.encode_string(self._parsed_data["vlan_id"]["nm"]), "nm")
             self._update_data(i, utils.encode_listofflags(self._parsed_data["vlan_id"]["mbr"], 8), "mbr")
             for k in ["piso", "lrn", "mrr", "igmp"]:
